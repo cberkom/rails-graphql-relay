1bb89972cbfb3602eab2caee0158dafe5da22966
Jason Waldrip <jason@waldrip.net>
2015-10-27 10:03:12 -0600

    Readable Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,15 @@
 10.times do
+  User.destroy_all
+
   user = User.create(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email
   )
 
-  5.times do
-    user.lists.create name: Faker::Internet.user_name
-  end
+  user.lists.create name: "Shopping List"
+  user.lists.create name: "Home Improvements"
+  user.lists.create name: "Weekly Tasks"
+  user.lists.create name: "Work Tasks"
+  user.lists.create name: "Yearly Goals"
 end
