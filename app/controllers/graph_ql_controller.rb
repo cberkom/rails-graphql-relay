a84632c645256ee46ab9ea759808d29edec21d06
Jason Waldrip <jason@waldrip.net>
2015-11-16 20:37:11 -0700

    add graphql formatter

--- app/controllers/graph_ql_controller.rb
+++ app/controllers/graph_ql_controller.rb
@@ -1,7 +1,7 @@
 class GraphQlController < ApplicationController
 
   def execute
-    puts '-' * 100, RelaySchema.prettify(params[:query]), '-' * 100
+    puts '-' * 100, GraphQLFormatter.new(params[:query]).to_s, '-' * 100
     result = RelaySchema.execute(params[:query], debug: true, variables: params[:variables])
     render json: result
   end
